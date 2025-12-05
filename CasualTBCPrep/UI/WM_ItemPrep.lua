2af65dc7ee655795bec2d69844c1e54c7db2dafb
Pawaox <pawaox@gmail.com>
2025-12-05 19:03:12 +0100

    minor thing

--- CasualTBCPrep/UI/WM_ItemPrep.lua
+++ CasualTBCPrep/UI/WM_ItemPrep.lua
@@ -2,6 +2,7 @@ CasualTBCPrep = CasualTBCPrep or {}
 CasualTBCPrep.WM_ItemPrep = CasualTBCPrep.WM_ItemPrep or {}
 
 --[Variables]
+---@class Frame|nil
 local frameItemPrep = nil;
 local _headerY = -31
 
