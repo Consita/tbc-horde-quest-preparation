d7570d5be4efc83d48636d6bb571a63a35eec2f8
Consita <jan.rechel@googlemail.com>
2025-12-17 12:42:59 +0100

    changed badlands to kargath

--- TBC/Quest-Preparation-Main.lua
+++ TBC/Quest-Preparation-Main.lua
@@ -3051,7 +3051,7 @@ RXPGuides.RegisterGuide([[
 			#completewith next
 			.goto Burning Steppes,65.6,24.2
 			>>|Tinterface/worldmap/chatbubble_64grey.blp:20|tTalk to |cRXP_FRIENDLY_Vahgruk|r
-			.fly Badlands >> Fly to |cFFfa9602Badlands|r	
+			.fly Badlands >> Fly to |cFFfa9602Kargath|r	
 			.target Vahgruk
 			.zoneskip Badlands
 
