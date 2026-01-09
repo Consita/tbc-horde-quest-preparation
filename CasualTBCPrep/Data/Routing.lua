c748ea9fae2d78134a548748ac9ae103b0697fb0
Consita <jan.rechel@googlemail.com>
2026-01-09 14:21:36 +0100

    fixing quest update after route change

--- CasualTBCPrep/Data/Routing.lua
+++ CasualTBCPrep/Data/Routing.lua
@@ -671,6 +671,7 @@ function CasualTBCPrep.Routing.ChangeCurrentRoute(routeCode)
     local oldRouteCode = CasualTBCPrep.Routing.CurrentRouteCode
     CasualTBCPrep.Routing.CurrentRouteCode = routeCode
 	CasualTBCPrep.QuestData.LoadRoute(routeCode)
+    CasualTBCPrep.QuestData.RefreshQuestProgression()
     cachedRouteQuestDic = CasualTBCPrep.Routing.GetQuestsInCurrentRoute()
 	CasualTBCPrep.WM_Route.RefreshRoute()
 
