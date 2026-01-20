927cbaf67cfd362fa0a3f11a4c9b7e26bde96b52
Pawaox <pawaox@gmail.com>
2026-01-20 23:33:37 +0100

    ElvUI Button change

--- CasualTBCPrep/CasualTBCPrep.lua
+++ CasualTBCPrep/CasualTBCPrep.lua
@@ -181,31 +181,9 @@ local function OnAddonLoadedEvent(self, event, addonName)
 			btnBank.textureObj = btnBankTexture
 			CasualTBCPrep.UI.HookTooltip(btnBank, tooltipHeader, ttCompanion, nil, funcCallHoverEnter, funcCallHoverLeave)
 			btnBank:SetScript("OnClick", funcToggleCompanion)
+
 			-- Button Bank Elvui
-			local f = CreateFrame("Frame")
-			f:RegisterEvent("BANKFRAME_OPENED")
-			f:SetScript("OnEvent", function()
-				C_Timer.After(0, function()
-					local bank = _G.ElvUI_BankContainerFrame
-					if not bank then return end
-
-					if _G.MyElvUIBankButton then
-						_G.MyElvUIBankButton:Show()
-						return
-					end
-
-					local btnBankElvui = CreateFrame("Button", "MyElvUIBankButton", bank)
-					btnBankElvui:SetPoint("TOPLEFT", bank, "TOPRIGHT", 0, 0)
-					btnBankElvui:SetSize(42, 42)
-					local btnBankElvuiTexture = btnBankElvui:CreateTexture(nil, "BORDER")
-					btnBankElvuiTexture:SetAllPoints(btnBankElvui)
-					btnBankElvuiTexture:SetTexture(CasualTBCPrep.AddonLogoTexture)
-					btnBankElvui.textureObj = btnBankElvuiTexture
-					CasualTBCPrep.UI.HookTooltip(btnBankElvui, tooltipHeader, ttCompanion, nil, funcCallHoverEnter, funcCallHoverLeave)
-					btnBankElvui:SetScript("OnClick", funcToggleCompanion)
-				end)
-			end)
-
+			CasualTBCPrep.ElvUI.RegisterBankOpen(tooltipHeader, ttCompanion, funcCallHoverEnter, funcCallHoverLeave, funcToggleCompanion)
 		end
 	end
 end
